1316677cb7c97737bb0f9523643f9e74282ca9a8
sivakumar1987 <7867793+sivakumar1987@users.noreply.github.com>
2020-04-27 18:10:27 +0530

    101 Adding teacher module

---
+++ teachers/src/main/java/com/example/teacher/TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.teacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+//test
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
